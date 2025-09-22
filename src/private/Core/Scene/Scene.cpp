7921c06c6d28688f200c2d8f25ca3e185f6a4c9e
PR3C14D0 <aarongarpre@gmail.com>
2025-09-22 14:07:22 +0200

    Scene: AddObject method created

--- src/private/Core/Scene/Scene.cpp
+++ src/private/Core/Scene/Scene.cpp
@@ -3,3 +3,26 @@
 Scene::Scene(std::string name) {
 	this->m_name = name;
 }
+
+void Scene::AddObject(GameObject* object) {
+	std::string objName = object->GetName();
+
+	if (this->m_gameObjects.count(this->m_name) > 0) {
+		spdlog::error("Scene::AddGameObject: GameObject with name {0} already exists", objName);
+		return;
+	}
+
+	this->m_gameObjects[objName] = object;
+}
+
+void Scene::Start() {
+	for (std::pair<std::string, GameObject*> obj : this->m_gameObjects) {
+		obj.second->Start();
+	}
+}
+
+void Scene::Update() {
+	for (std::pair<std::string, GameObject*> obj : this->m_gameObjects) {
+		obj.second->Update();
+	}
+}
