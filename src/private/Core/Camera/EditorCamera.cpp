1064551a71c557e8c168b6326547a4b94052fe7f
PR3C14D0 <aarongarpre@gmail.com>
2025-12-13 03:39:18 +0100

    feat: Implement basic EditorCamera functionality

---
+++ src/private/Core/Camera/EditorCamera.cpp
@@ -0,0 +1,13 @@
+#include "Core/Camera/EditorCamera.h"
+
+EditorCamera::EditorCamera(std::string name) : Camera::Camera(name) {
+	
+}
+
+void EditorCamera::Start() {
+	Camera::Start();
+}
+
+void EditorCamera::Update() {
+	Camera::Update();
+}
