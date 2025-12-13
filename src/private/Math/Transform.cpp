b7b0ce61dc12dd483fdaab39435bb40f1f6a70c8
PR3C14D0 <aarongarpre@gmail.com>
2025-12-13 15:15:47 +0100

    Math/Transform: Add rotation capabilities and directional vectors

--- src/private/Math/Transform.cpp
+++ src/private/Math/Transform.cpp
@@ -16,6 +16,40 @@ void Transform::Translate(float v) {
 	this->Translate(v, v, v);
 }
 
-void Transform::Translate(Vector3& v) {
+void Transform::Translate(Vector3 v) {
 	this->Translate(v.x, v.y, v.z);
+}
+
+void Transform::Rotate(float x, float y, float z) {
+	this->rotation.x += x;
+	this->rotation.y += y;
+	this->rotation.z += z;
+}
+
+void Transform::Rotate(float v) {
+	this->Rotate(v, v, v);
+}
+
+void Transform::Rotate(Vector3 v) {
+	this->Rotate(v.x, v.y, v.z);
+}
+
+Vector3 Transform::Forward() {
+	Vector3 v = Vector3{ 0.f, 0.f, 1.f };
+	return this->RotatePoint(v);
+}
+
+Vector3 Transform::Right() {
+	Vector3 v = Vector3{ 1.f, 0.f, 0.f };
+	return this->RotatePoint(v);
+}
+
+Vector3 Transform::RotatePoint(Vector3& point) {
+	glm::mat4 rot = glm::mat4(1.f);
+	rot = glm::rotate(rot, glm::radians(this->rotation.y), { 0.f, 1.f, 0.f });
+	rot = glm::rotate(rot, glm::radians(this->rotation.x), { 1.f, 0.f, 0.f });
+	rot = glm::rotate(rot, glm::radians(this->rotation.z), { 0.f, 0.f, 1.f });
+
+	glm::vec4 rotToPoint = rot * glm::vec4(point.x, point.y, point.z, 0.f);
+	return { rotToPoint.x, rotToPoint.y, rotToPoint.z };
 }
--- src/public/Math/Transform.h
+++ src/public/Math/Transform.h
@@ -2,6 +2,10 @@
 #include <iostream>
 #include "Math/Vector3.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+
 struct Transform {
 	Vector3 location;
 	Vector3 rotation;
@@ -11,5 +15,14 @@ struct Transform {
 
 	void Translate(float x, float y, float z);
 	void Translate(float v);
-	void Translate(Vector3& v);
+	void Translate(Vector3 v);
+
+	void Rotate(float x, float y, float z);
+	void Rotate(float v);
+	void Rotate(Vector3 v);
+
+	Vector3 Forward();
+	Vector3 Right();
+
+	Vector3 RotatePoint(Vector3& point);
 };
