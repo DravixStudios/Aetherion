07dd3b8662064e912711c1a2daaa421bffc944ce
PR3C14D0 <aarongarpre@gmail.com>
2025-12-16 15:36:53 +0100

    refactor(vulkan): Introduce explicit usageFlags variable in CreateTexture

--- src/private/Core/Renderer/Vulkan/VulkanRenderer.cpp
+++ src/private/Core/Renderer/Vulkan/VulkanRenderer.cpp
@@ -2532,12 +2532,15 @@ GPUTexture* VulkanRenderer::CreateTexture(GPUBuffer* pBuffer, uint32_t nWidth, u
 	*/
 	VkDeviceMemory imageMemory = nullptr;
 	VkImage image = nullptr;
+
+	VkImageUsageFlags usageFlags = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+
 	uint32_t nImageSize = this->CreateImage(
 		nWidth, nHeight,
 		vkFormat,
 		VK_SAMPLE_COUNT_1_BIT,
 		VK_IMAGE_TILING_OPTIMAL,
-		VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+		usageFlags,
 		VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
 		image,
 		imageMemory);
