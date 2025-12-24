6838486c1f0d267b1e2fd3e2d32ddec0f6817529
PR3C14D0 <aarongarpre@gmail.com>
2025-12-24 18:02:17 +0100

    feat: Implement BRDF render pass creation

--- src/private/Core/Renderer/Vulkan/VulkanRenderer.cpp
+++ src/private/Core/Renderer/Vulkan/VulkanRenderer.cpp
@@ -1051,7 +1051,43 @@ void VulkanRenderer::CreatePrefilterRenderPass() {
 
 /* Create BRDF render pass */
 void VulkanRenderer::CreateBRDFRenderPass() {
+	/* Attachment 0: Color attachment */
+	VkAttachmentDescription colorAttachment = { };
+	colorAttachment.format = VK_FORMAT_R32G32_SFLOAT; // Only 2 channels (RG)
+	colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+	colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+	colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+	colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	colorAttachment.finalLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
 
+	/* Color attachment reference */
+	VkAttachmentReference colorRef = { };
+	colorRef.attachment = 0;
+	colorRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+	/* Subpass description */
+	VkSubpassDescription subpass = { };
+	subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+	subpass.colorAttachmentCount = 1;
+	subpass.pColorAttachments = &colorRef;
+
+	/* Render pass create info */
+	VkRenderPassCreateInfo createInfo = { };
+	createInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+	createInfo.attachmentCount = 1;
+	createInfo.pAttachments = &colorAttachment;
+	createInfo.subpassCount = 1;
+	createInfo.pSubpasses = &subpass;
+
+	if (vkCreateRenderPass(this->m_device, &createInfo, nullptr, &this->m_brdfRenderPass) != VK_SUCCESS) {
+		spdlog::error("VulkanRenderer::CreateBRDFRenderPass: Failed creating BRDF render pass");
+		throw std::runtime_error("VulkanRenderer::CreateBRDFRenderPass: Failed creating BRDF render pass");
+		return;
+	}
+
+	spdlog::debug("VulkanRenderer::CreateBRDFRenderPass: BRDF render pass created");
 }
 
 /* Command pool creation */
