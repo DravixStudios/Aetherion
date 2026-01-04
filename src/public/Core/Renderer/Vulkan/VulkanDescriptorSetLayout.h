c4b7dd11110f20cc927e8b805f0f9f1c4f081594
PR3C14D0 <aarongarpre@gmail.com>
2026-01-04 21:03:15 +0100

    feat: started with vulkan descriptor set layout implementation

---
+++ src/public/Core/Renderer/Vulkan/VulkanDescriptorSetLayout.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Core/Renderer/DescriptorSetLayout.h"
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+class VulkanDescriptorSetLayout : public DescriptorSetLayout {
+public:
+	VulkanDescriptorSetLayout(VkDevice);
+	~VulkanDescriptorSetLayout() override;
+
+	void Create(const DescriptorSetLayoutCreateInfo& createInfo) override;
+
+	VkDescriptorSetLayout GetVkLayout() const { return this->m_layout; }
+private:
+	VkDevice m_device;
+	VkDescriptorSetLayout m_layout;
+
+	VkDescriptorType ConvertDescriptorType(EDescriptorType type);
+	VkShaderStageFlags ConvertShaderStage(EShaderStage stage);
+};
