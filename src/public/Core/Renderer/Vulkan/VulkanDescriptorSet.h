cf1fabf47d74d0cdbe3408b7f1ac3c78908ad01d
PR3C14D0 <aarongarpre@gmail.com>
2026-01-05 04:17:19 +0100

    feat: started with vulkan descriptor set abstraction

---
+++ src/public/Core/Renderer/Vulkan/VulkanDescriptorSet.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Core/Renderer/DescriptorSet.h"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+class VulkanDescriptorSet : public DescriptorSet {
+public:
+	using Ptr = Ref<VulkanDescriptorSet>;
+
+	explicit VulkanDescriptorSet(VkDevice device);
+	~VulkanDescriptorSet() override;
+
+	void Allocate(Ref<DescriptorPool> pool, Ref<DescriptorSetLayout> layout) override;
+
+	void WriteBuffer(uint32_t nBinding, uint32_t nArrayElement, const DescriptorBufferInfo& bufferInfo) override;
+	void WriteTexture(uint32_t nBinding, uint32_t nArrayElement, const DescriptorImageInfo& imageInfo) override;
+
+	void UpdateWrites() override;
+
+	VkDescriptorSet GetVkSet() const { return this->m_descriptorSet; }
+
+	static Ptr
+	CreateShared(VkDevice device) {
+		return CreateRef<VulkanDescriptorSet>(device);
+	}
+private:
+	VkDevice m_device;
+	VkDescriptorSet m_descriptorSet;
+};
