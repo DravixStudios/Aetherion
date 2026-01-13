7c335d0c6ae8b96d481e2ad41a211ddb987e54c9
PR3C14D0 <aarongarpre@gmail.com>
2026-01-13 03:04:19 +0100

    feat: VulkanGraphicsContext BindPipeline method defined

--- src/private/Core/Renderer/Vulkan/VulkanGraphicsContext.cpp
+++ src/private/Core/Renderer/Vulkan/VulkanGraphicsContext.cpp
@@ -4,4 +4,16 @@ VulkanGraphicsContext::VulkanGraphicsContext(VkCommandBuffer commandBuffer)
 	: m_commandBuffer(commandBuffer), m_currentPipeline(VK_NULL_HANDLE), 
 	m_currentBindPoint(VK_PIPELINE_BIND_POINT_MAX_ENUM) {}
 
+/**
+* Binds a Vulkan pipeline
+* 
+* @param pipeline Binded pipeline
+*/
+void 
+VulkanGraphicsContext::BindPipeline(Ref<Pipeline> pipeline) {
+	Ref<VulkanPipeline> vkPipeline = pipeline.As<VulkanPipeline>();
+	this->m_currentPipeline = vkPipeline->GetVkPipeline();
+	this->m_currentBindPoint = vkPipeline->GetVkBindPoint();
 
+	vkCmdBindPipeline(this->m_commandBuffer, this->m_currentBindPoint, this->m_currentPipeline);
+}
--- src/public/Core/Renderer/Vulkan/VulkanGraphicsContext.h
+++ src/public/Core/Renderer/Vulkan/VulkanGraphicsContext.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Core/Renderer/GraphicsContext.h"
 
+#include "Core/Renderer/Vulkan/VulkanPipeline.h"
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
