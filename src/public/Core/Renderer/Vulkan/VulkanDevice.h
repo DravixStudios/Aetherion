4baf5a6127d6a68a0e9671fc74b93e46e61a428e
PR3C14D0 <aarongarpre@gmail.com>
2026-01-13 11:19:27 +0100

    Revert "feat: QueueFamilyIndices struct defined"
    This reverts commit cc9a69a631c82b01e98771371c77dc37aaff8ee6.

--- src/public/Core/Renderer/Vulkan/VulkanDevice.h
+++ src/public/Core/Renderer/Vulkan/VulkanDevice.h
@@ -1,11 +1,8 @@
 #pragma once
 #include "Core/Renderer/Device.h"
-#include <optional>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-struct QueueFamilyIndices;
 
 class VulkanDevice : public Device {
 public:
