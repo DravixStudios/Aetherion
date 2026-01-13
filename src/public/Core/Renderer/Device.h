6ff74aa2a74d02f1f0d1cf084878ebec390fc056
PR3C14D0 <aarongarpre@gmail.com>
2026-01-13 10:29:46 +0100

    feat: Device abstract class methods declared

--- src/public/Core/Renderer/Device.h
+++ src/public/Core/Renderer/Device.h
@@ -31,7 +31,60 @@ struct DeviceCreateInfo {
 
 class Device {
 public:
+	static constexpr const char* CLASS_NAME = "Device";
+
+	using Ptr = Ref<Device>;
+
 	virtual ~Device();
 
+	/**
+	* Creates a logical device
+	* 
+	* @param createInfo Device create info
+	*/
+	virtual void Create(const DeviceCreateInfo& createInfo) = 0;
 
+	/**
+	* Waits till device finishes all the operations
+	*/
+	virtual void WaitIdle() = 0;
+
+	/**
+	* Gets queue family information by type
+	* 
+	* @param queueType Queue type
+	* 
+	* @returns Queue family info
+	*/
+	virtual QueueFamilyInfo GetQueueFamilyInfo(EQueueType queueType) const = 0;
+	
+	/**
+	*	Gets a queue family index that supports the specified operations
+	* 
+	* @param bGraphics Needs graphics support
+	* @param bCompute Needs compute support
+	* @param bTransfer Needs transfer support
+	* @param bPresent Needs present support
+	*/
+	virtual uint32_t FindQueueFamily(
+		bool bGraphics = false,
+		bool bCompute = false,
+		bool bTransfer = false,
+		bool bPresent = false
+	) = 0;
+
+	/**
+	* Gets device limits
+	*/
+	virtual void GetLimits(
+		uint32_t& nMaxUniformBufferRange,
+		uint32_t& nMaxStorageBufferRange,
+		uint32_t& nMaxPushContantsSize,
+		uint32_t& nMaxBoundDescriptorSets
+	) const = 0;
+
+	/**
+	* Device name
+	*/
+	virtual const char* GetDeviceName() const = 0;
 };
