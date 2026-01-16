629e6d2543ed6fb97d4fc574657aeeb466bd7431
PR3C14D0 <aarongarpre@gmail.com>
2026-01-16 14:33:45 +0100

    refactor: refactored GPUTexture abstract class

--- src/public/Core/Renderer/GPUTexture.h
+++ src/public/Core/Renderer/GPUTexture.h
@@ -1,17 +1,121 @@
 #pragma once
 #include <iostream>
 
-enum ETextureType {
-	TEXTURE = 0,
-	CUBEMAP = 1
+#include "Core/Containers.h"
+#include "Core/Renderer/Extent3D.h"
+#include "Core/Renderer/GPUFormat.h"
+
+enum class ETextureType {
+	TEXTURE,
+	CUBEMAP,
+	UNDEFINED
+};
+
+enum class ETextureDimensions {
+	TYPE_1D,
+	TYPE_2D,
+	TYPE_3D
+};
+
+enum class ETextureFlags : uint32_t {
+	SPARSE_BINDING = 1,
+	SPARSE_RESIDENCY = 1 << 1,
+	SPARSE_ALIASED = 1 << 2,
+	MUTABLE_FORMAT = 1 << 3,
+	CUBE_COMPATIBLE = 1 << 4
+};
+
+inline ETextureFlags 
+operator|(ETextureFlags a, ETextureFlags b) {
+	return static_cast<ETextureFlags>(static_cast<uint32_t>(a) | static_cast<uint32_t>(b));
+}
+
+inline ETextureFlags
+operator&(ETextureFlags a, ETextureFlags b) {
+	return static_cast<ETextureFlags>(static_cast<uint32_t>(a) & static_cast<uint32_t>(b));
+}
+
+enum class ESampleCount {
+	SAMPLE_1,
+	SAMPLE_2,
+	SAMPLE_4,
+	SAMPLE_8,
+	SAMPLE_16,
+	SAMPLE_32,
+	SAMPLE_64
+};
+
+enum class ETextureTiling {
+	OPTIMAL,
+	LINEAR
+};
+
+enum class ETextureUsage : uint32_t {
+	TRANSFER_SRC = 1,
+	TRANSFER_DST = 1 << 2,
+	SAMPLED = 1 << 3,
+	STORAGE = 1 << 4,
+	COLOR_ATTACHMENT = 1 << 5,
+	DEPTH_STENCIL_ATTACHMENT = 1 << 6,
+	TRANSIENT_ATTACHMENT = 1 << 7,
+	INPUT_ATTACHMENT = 1 << 8
+};
+
+inline ETextureUsage
+operator|(ETextureUsage a, ETextureUsage b) {
+	return static_cast<ETextureUsage>(static_cast<uint32_t>(a) | static_cast<uint32_t>(b));
+}
+
+inline ETextureUsage
+operator&(ETextureUsage a, ETextureUsage b) {
+	return static_cast<ETextureUsage>(static_cast<uint32_t>(a) & static_cast<uint32_t>(b));
+}
+
+enum class ESharingMode {
+	EXCLUSIVE,
+	CONCURRENT
+};
+
+enum class EImageLayout {
+	UNDEFINED,
+	GENERAL,
+	COLOR_ATTACHMENT_OPTIMAL,
+	DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+	DEPTH_STENCIL_READ_ONLY_OPTIMAL,
+	SHADER_READ_ONLY_OPTIMAL,
+	TRANSFER_SRC_OPTIMAL,
+	TRANSFER_DST_OPTIMAL,
+	PREINITIALIZED
+};
+
+struct TextureCreateInfo {
+	Ref<GPUBuffer> buffer;
+	ETextureFlags flags;
+	ETextureDimensions imageType;
+	GPUFormat format;
+	Extent3D extent;
+	uint32_t nMipLevels;
+	uint32_t nArrayLayers;
+	ESampleCount samples;
+	ETextureTiling tiling;
+	ETextureUsage usage;
+	ESharingMode sharingMode;
+	uint32_t nQueueFamilyIndexCount;
+	const uint32_t* pQueueFamilyIndices;
+	EImageLayout initialLayout;
 };
 
 class GPUTexture {
 public:
-	GPUTexture(ETextureType textureType = ETextureType::TEXTURE) { this->textureType = textureType; }
-	~GPUTexture() {}
-
-	virtual uint32_t GetSize() = 0;
+	static constexpr const char* CLASS_NAME = "GPUTexture";
 	
-	ETextureType textureType;
+	using Ptr = Ref<GPUTexture>;
+
+	virtual ~GPUTexture() = default;
+
+	virtual void Create(const TextureCreateInfo& createInfo) = 0;
+
+	virtual uint32_t GetSize() const = 0;
+
+	ETextureType textureType = ETextureType::UNDEFINED;
 };
