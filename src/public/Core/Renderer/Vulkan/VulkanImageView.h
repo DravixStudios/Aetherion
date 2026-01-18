7bbd548f6eae1d789361fb132a1ec7980dec5d0e
PR3C14D0 <aarongarpre@gmail.com>
2026-01-18 03:32:30 +0100

    feat: VulkanImageView::Create method finished

---
+++ src/public/Core/Renderer/Vulkan/VulkanImageView.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Core/Renderer/ImageView.h"
+
+#include "Core/Renderer/Vulkan/VulkanHelpers.h"
+#include "Core/Renderer/Vulkan/VulkanTexture.h"
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+class VulkanImageView : public ImageView {
+public:
+	using Ptr = Ref<VulkanImageView>;
+
+	explicit VulkanImageView(VkDevice device);
+	~VulkanImageView() override;
+
+	void Create(const ImageViewCreateInfo& createInfo) override;
+
+	Ref<GPUTexture> GetImage() const override;
+	EImageViewType GetViewType() const override;
+	GPUFormat GetFormat() const override;
+
+	static Ptr
+	CreateShared(VkDevice device) {
+		return CreateRef<VulkanImageView>(device);
+	}
+
+private:
+	VkDevice m_device;
+
+	VkImageView m_imageView;
+
+	VkComponentSwizzle ConvertSwizzle(ComponentMapping::ESwizzle swizzle);
+};
