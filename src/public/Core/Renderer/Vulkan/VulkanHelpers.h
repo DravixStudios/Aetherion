e1b4d90bac74019c93dc70de0fdeb5ad12788c47
PR3C14D0 <aarongarpre@gmail.com>
2026-01-19 04:38:35 +0100

    feat: VulkanHelpers RevertFormat helper function created

--- src/public/Core/Renderer/Vulkan/VulkanHelpers.h
+++ src/public/Core/Renderer/Vulkan/VulkanHelpers.h
@@ -272,6 +272,22 @@ namespace VulkanHelpers {
 		}
 	}
 
+	inline GPUFormat
+	RevertFormat(VkFormat format) {
+		switch (format) {
+			case VK_FORMAT_R8G8B8A8_UNORM: return GPUFormat::RGBA8_UNORM;
+			case VK_FORMAT_B8G8R8A8_UNORM: return GPUFormat::BGRA8_UNORM;
+			case VK_FORMAT_R8G8B8A8_SRGB: return GPUFormat::RGBA8_SRGB;
+			case VK_FORMAT_R16G16B16A16_SFLOAT: return GPUFormat::RGBA16_FLOAT;
+			case VK_FORMAT_R32G32B32A32_SFLOAT: return GPUFormat::RGBA32_FLOAT;
+			case VK_FORMAT_D24_UNORM_S8_UINT: return GPUFormat::D24_UNORM_S8_UINT;
+			case VK_FORMAT_D32_SFLOAT: return GPUFormat::D32_FLOAT;
+			case VK_FORMAT_D32_SFLOAT_S8_UINT: return GPUFormat::D32_FLOAT_S8_UINT;
+			case VK_FORMAT_R8_UNORM: return GPUFormat::R8_UNORM;
+			default: return GPUFormat::RGBA8_UNORM;
+		}
+	}
+
 	inline VkCullModeFlags
 	ConvertCullMode(ECullMode cullMode) {
 		switch (cullMode) {
