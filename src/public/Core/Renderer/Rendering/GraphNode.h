8e3f1ed4000fbc9c1c749d8c180a5f79ff8a3b96
PR3C14D0 <aarongarpre@gmail.com>
2026-02-03 14:03:51 +0100

    feat: GraphNode struct created

---
+++ src/public/Core/Renderer/Rendering/GraphNode.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Core/Containers.h"
+#include "Core/Renderer/GraphicsContext.h"
+#include "Core/Renderer/RenderPass.h"
+#include "Core/Renderer/Framebuffer.h"
+#include "Core/Renderer/Rendering/ResourceHandle.h"
+
+#include <functional>
+
+class RenderGraphContext;
+
+struct GraphNode {
+	const char* name = nullptr;
+
+	Vector<TextureHandle> colorOutputs;
+	TextureHandle depthOutput;
+	bool bHasDepth = false;
+	Vector<TextureHandle> textureInputs;
+
+	Ref<RenderPass> renderPass;
+	Ref<Framebuffer> framebuffer;
+
+	uint32_t nWidth = 0;
+	uint32_t nHeight = 0;
+
+	std::function<void(GraphicsContext&, RenderGraphContext&)> execute;
+
+	Vector<uint32_t> dependsOn; // Indices of the nodes that depends on
+};
