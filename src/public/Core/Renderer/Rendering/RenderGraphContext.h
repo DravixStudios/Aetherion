19c54eff14d273d97a10d70e8f58fc456526c0b8
PR3C14D0 <aarongarpre@gmail.com>
2026-02-03 14:08:29 +0100

    feat: RenderGraphContext class declared

---
+++ src/public/Core/Renderer/Rendering/RenderGraphContext.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Core/Renderer/GPUTexture.h"
+#include "Core/Renderer/ImageView.h"
+#include "Core/Renderer/Rendering/ResourceHandle.h"
+
+class TransientResourcePool;
+
+class RenderGraphContext {
+public:
+	Ref<GPUTexture> GetTexutre(TextureHandle handle);
+	Ref<ImageView> GetImageView(TextureHandle handle);
+
+private:
+	friend class RenderGraph;
+	Ref<TransientResourcePool> m_pool;
+};
