f7048fd6f0902babb00316577e2acfa9a90143a2
PR3C14D0 <aarongarpre@gmail.com>
2026-02-05 16:23:06 +0100

    feat: EAccess enumerator created

--- src/public/Core/Renderer/GPUBuffer.h
+++ src/public/Core/Renderer/GPUBuffer.h
@@ -39,6 +39,37 @@ enum class EIndexType {
 	UINT32
 };
 
+enum class EAccess : uint32_t {
+	NONE = 0,
+	INDIRECT_COMMAND_READ = 1,
+	INDEX_READ = 1 << 1,
+	VERTEX_ATTRIBUTE_READ = 1 << 2,
+	UNIFORM_READ = 1 << 3,
+	INPUT_ATTACHMENT_READ = 1 << 4,
+	SHADER_READ = 1 << 5,
+	SHADER_WRITE = 1 << 6,
+	COLOR_ATTACHMENT_READ = 1 << 7,
+	COLOR_ATTACHMENT_WRITE = 1 << 8,
+	DEPTH_STENCIL_READ = 1 << 9,
+	DEPTH_STENCIL_WRITE =  1 << 10,
+	TRANSFER_READ = 1 << 11,
+	TRANSFER_WRITE = 1 << 12,
+	HOST_READ = 1 << 13,
+	HOST_WRITE = 1 << 14,
+	MEMORY_READ = 1 << 15,
+	MEMORY_WRITE =  1 << 16
+};
+
+inline EAccess
+operator|(EAccess a, EAccess b) {
+	return static_cast<EAccess>(static_cast<uint32_t>(a) | static_cast<uint32_t>(b));
+}
+
+inline EAccess
+operator&(EAccess a, EAccess b) {
+	return static_cast<EAccess>(static_cast<uint32_t>(a) & static_cast<uint32_t>(b));
+}
+
 class GPUBuffer {
 public:
 	static constexpr const char* CLASS_NAME = "GPUBuffer";
