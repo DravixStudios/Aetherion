21e1c388d82a32a10a4dcfdfbf55d8a9fb74ad24
PR3C14D0 <aarongarpre@gmail.com>
2026-02-07 02:57:23 +0100

    refactor: ResourceManager class refactor

--- src/public/Core/Renderer/ResourceManager.h
+++ src/public/Core/Renderer/ResourceManager.h
@@ -1,31 +1,19 @@
 #pragma once
-#include <iostream>
 #include <map>
 
-#include <spdlog/spdlog.h>
-
 #include "Core/Containers.h" 
-#include "Core/Renderer/GPUBuffer.h"
-#include "Core/Renderer/GPUTexture.h"
-
-class Renderer;
 
 class ResourceManager {
 public:
-	ResourceManager();
+	ResourceManager() = default;
 
-	GPUTexture* GetTexture(String textureName);
-	bool AddTexture(String textureName, GPUTexture* pTexture);
-	bool TextureExists(String textureName);
-
-	GPUTexture* UploadTexture(String textureName, const unsigned char* pData, int nWidth, int nHeight);
-
-	void SetRenderer(Renderer* pRenderer);
+	bool IsTextureRegistered(const String& name) const;
+	void RegisterTexture(const String& name, uint32_t nBindlessIndex);
+	uint32_t GetTextureIndex(const String& name) const;
 
 	static ResourceManager* GetInstance();
 private:
-	std::map<String, GPUTexture*> m_textures;
-	Renderer* m_renderer;
-
 	static ResourceManager* m_instance;
+
+	std::map<String, uint32_t> m_textureIndices;
 };
