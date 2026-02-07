cb526aef48ef05fbf9b53dd9e5c278d3b9bfed97
PR3C14D0 <aarongarpre@gmail.com>
2026-02-07 02:08:13 +0100

    feat: Scene::CollectDrawData method created

--- src/public/Core/Scene/Scene.h
+++ src/public/Core/Scene/Scene.h
@@ -4,8 +4,10 @@
 #include <spdlog/spdlog.h>
 
 #include "Core/GameObject/GameObject.h"
+#include "Core/GameObject/Components/Mesh.h"
 #include "Core/Camera/Camera.h"
 #include "Core/Camera/EditorCamera.h"
+#include "Utils.h"
 
 class Scene {
 	friend class SceneManager;
@@ -16,6 +18,13 @@ public:
 	std::map<String, GameObject*> GetObjects();
 
 	Camera* GetCurrentCamera();
+
+	void CollectDrawData(
+		Vector<ObjectInstanceData>& instances,
+		Vector<DrawBatch>& batches,
+		Vector<glm::mat4>& wvpMatrices,
+		const glm::mat4 viewProj
+	);
 
 	void Start();
 	void Update();
