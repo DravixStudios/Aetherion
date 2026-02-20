85775f9e66bb5b6651cc20f5a8f14e65f8aa4ee5
PR3C14D0 <aarongarpre@gmail.com>
2026-02-20 01:11:26 +0100

    feat: MegaBuffer::Block struct created

--- src/public/Core/Renderer/MegaBuffer.h
+++ src/public/Core/Renderer/MegaBuffer.h
@@ -4,6 +4,7 @@
 #include "Core/Renderer/GPUBuffer.h"
 
 struct MegaBufferAllocation {
+	uint32_t nBlockIndex;
 	uint32_t nVertexOffset;
 	uint32_t nFirstIndex;
 	uint32_t nIndexCount;
@@ -11,6 +12,20 @@ struct MegaBufferAllocation {
 
 class MegaBuffer {
 public:
+	struct Block {
+		Ref<GPUBuffer> vertexBuffer;
+		Ref<GPUBuffer> indexBuffer;
+
+		void* pVertexMap = nullptr;
+		void* pIndexMap = nullptr;
+
+		uint32_t nMaxVertices = 0;
+		uint32_t nMaxIndices = 0;
+
+		uint32_t nCurrentVertexOffset = 0;
+		uint32_t nCurrentIndexOffset = 0;
+	};
+
 	void Init(Ref<Device> device, uint32_t nMaxVertices, uint32_t nMaxIndices);
 	MegaBufferAllocation Upload(const Vector<Vertex>& vertices, const Vector<uint16_t>& indices);
 
