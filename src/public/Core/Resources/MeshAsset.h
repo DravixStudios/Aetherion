a3aef635060490867673d93765dede0dc160627b
PR3C14D0 <aarongarpre@gmail.com>
2026-03-02 13:24:58 +0100

    feat: MeshAsset struct created

---
+++ src/public/Core/Resources/MeshAsset.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Core/Containers.h"
+
+struct MeshAssetHeader {
+	uint32_t nVertexCount;
+	uint32_t nVertexOffset;
+	uint32_t nIndexCount;
+	uint32_t nIndexOffset;
+	uint32_t nTotalByteSize;
+	uint32_t nNameSize;
+	Name displayName;
+};
+
+struct MeshAsset {
+	MeshAssetHeader header;
+	const void* pcData = nullptr;
+};
