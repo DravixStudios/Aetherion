9a79464cede9b8eab5040e26c98c2d5af793b4a2
PR3C14D0 <aarongarpre@gmail.com>
2026-05-27 23:01:22 +0200

    feat: Implement scene serialization

--- src/private/Core/Scene/Scene.cpp
+++ src/private/Core/Scene/Scene.cpp
@@ -1,4 +1,6 @@
 #include "Core/Scene/Scene.h"
+#include "Core/Resources/SceneAsset.h"
+#include "Core/Resources/GameObjectAsset.h"
 
 Scene::Scene(String name) {
 	this->m_name = name;
@@ -41,4 +43,56 @@ Scene::Update() {
 		obj.second->Update();
 	}
 	this->currentCamera->Update();
+}
+
+/**
+* Serialize scene data
+* 
+* @returns Serialized scene asset
+*/
+const SceneAsset 
+Scene::SerializeScene() {
+	/* Get GameObject count */
+	uint32_t nObjectCount = this->m_gameObjects.size();
+	std::vector<GameObjectAsset> assets(nObjectCount);
+
+	SceneAsset sceneAsset = { };
+
+	/* Serialize GameObjects */
+	uint32_t i = 0;
+	for (auto& [name, pObj] : this->m_gameObjects) {
+
+		/* Create a GameObjectAsset */
+		GameObjectAsset objAsset = { };
+		objAsset.transform = pObj->transform;
+
+		/* Serialize mesh asset */
+		Map<String, Component*> components =  pObj->GetComponents();
+		if (components.contains("Mesh")) {
+			Mesh* meshComponent = dynamic_cast<Mesh*>(components["Mesh"]);
+
+			if (meshComponent) {
+				objAsset.header.displayName = pObj->GetName();
+				objAsset.components = objAsset.components | EAssetComponent::MESH;
+				objAsset.transform = pObj->transform;
+
+				AssetHandle meshHandle = AssetHandle::FromPath(meshComponent->GetFilePath(), EAssetType::MESH);
+				objAsset.meshHandle = meshHandle;
+			}	
+		}
+		
+		assets[i] = std::move(objAsset);
+		i++;
+	}
+
+	if (i != nObjectCount) {
+		Logger::Error("Scene::SerializeScene: Object count mismatch. Got {} of {}", i, nObjectCount);
+		return SceneAsset{};
+	}
+
+	sceneAsset.header.displayName = this->GetName();
+	sceneAsset.header.nObjectCount = nObjectCount;
+	sceneAsset.objects = std::move(assets);
+
+	return sceneAsset;
 }
